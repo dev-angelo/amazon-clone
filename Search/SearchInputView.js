0cbc56fdc0b8cf5a2200e546b19dce4ab85b4d07
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-02-26 20:53:58 +0900

    test

--- Search/SearchInputView.js
+++ Search/SearchInputView.js
@@ -8,7 +8,6 @@ class SearchInputView {
         <div class="searchBox">
             <input type="text" class="searchInputField" placeholder="What are you looking for?">
             <button type="submit" class="searchButton">
-                <i class="fa fa-search"></i>
             </button>
         </div>
         `
