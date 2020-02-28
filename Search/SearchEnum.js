dfcf9476a9bbd44b664a68da095849e4ac32e76f
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-02-28 22:56:52 +0900

    최근 검색어 기능 추가

--- Search/SearchEnum.js
+++ Search/SearchEnum.js
@@ -5,6 +5,17 @@ const SEARCH_AJAX_INFORMATION = Object.freeze({
 const SEARCH_STATUS = Object.freeze({
     NORMAL: 0,
     AUTOCOMPLETION: 1,
+    RECENT_SEARCH: 2
 });
 
-export default {SEARCH_AJAX_INFORMATION, SEARCH_STATUS};
+const SEARCH_TIMEOUT = Object.freeze({
+    REQUEST: 500,
+    CACHED: 300,
+});
+
+const SEARCH_CACHE_INFORMATION = Object.freeze({
+    KEY: '<cached-data>',
+    MAX_CACHE_COUNT: 5,
+});
+
+export default {SEARCH_AJAX_INFORMATION, SEARCH_STATUS, SEARCH_TIMEOUT, SEARCH_CACHE_INFORMATION};
